c111afb85f8d3eec29e92a08967af0bc465869f3
Jaari <jaari.rey@gmail.com>
2024-07-07 16:23:06 +1000

    added bishop tests

--- BishopTests.cs
+++ BishopTests.cs
@@ -1,11 +1,6 @@
 ﻿using Chess.Classes;
 using Chess.Classes.ConcretePieces;
 using Chess.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChessTests
 {
