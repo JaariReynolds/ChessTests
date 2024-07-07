08528376ffab5a75c0f5ced983ce7e923009ffcd
Jaari <jaari.rey@gmail.com>
2024-07-07 16:24:18 +1000

    Revert "added bishop tests"
    This reverts commit c111afb85f8d3eec29e92a08967af0bc465869f3.

--- BishopTests.cs
+++ BishopTests.cs
@@ -1,6 +1,11 @@
 ﻿using Chess.Classes;
 using Chess.Classes.ConcretePieces;
 using Chess.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ChessTests
 {
