21a1c3acda668ca8076f4e43b13171c64ff1a57d
Jaari <jaari.rey@gmail.com>
2024-07-07 16:20:12 +1000

    added bishop tests

---
+++ BishopTests.cs
@@ -0,0 +1,347 @@
+﻿using Chess.Classes;
+using Chess.Classes.ConcretePieces;
+using Chess.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTests
+{
+    public class BishopTests
+    {
+        /// <summary>
+        /// A bishop should be able to move freely in any diagonal direction
+        /// </summary>
+        [Fact]
+        public void BishopWhiteMove()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 0, 0, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Move),
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 6, 6, ActionType.Move),
+                new Action(bishop, 7, 7, ActionType.Move),
+                new Action(bishop, 6, 0, ActionType.Move),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 1, 5, ActionType.Move),
+                new Action(bishop, 0, 6, ActionType.Move),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should be able to move freely in any diagonal direction
+        /// </summary>
+        [Fact]
+        public void BishopBlackMove()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.Black, 4, 3);
+
+            gameboard.SwapTurns();
+            gameboard.SetTestBoard(4, 3, bishop);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 1, 0, ActionType.Move),
+                new Action(bishop, 2, 1, ActionType.Move),
+                new Action(bishop, 3, 2, ActionType.Move),
+                new Action(bishop, 5, 4, ActionType.Move),
+                new Action(bishop, 6, 5, ActionType.Move),
+                new Action(bishop, 7, 6, ActionType.Move),
+                new Action(bishop, 7, 0, ActionType.Move),
+                new Action(bishop, 6, 1, ActionType.Move),
+                new Action(bishop, 5, 2, ActionType.Move),
+                new Action(bishop, 3, 4, ActionType.Move),
+                new Action(bishop, 2, 5, ActionType.Move),
+                new Action(bishop, 1, 6, ActionType.Move),
+                new Action(bishop, 0, 7, ActionType.Move),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should not be able to move past a friendly piece (north west)
+        /// </summary>
+        [Fact]
+        public void BishopObstructionNorthWest()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var friendlyPawn1 = new Pawn(TeamColour.White, 1, 1);
+            var friendlyPawn2 = new Pawn(TeamColour.White, 2, 2);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(1, 1, friendlyPawn1);
+            gameboard.SetTestBoard(2, 2, friendlyPawn2);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 6, 6, ActionType.Move),
+                new Action(bishop, 7, 7, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 1, 5, ActionType.Move),
+                new Action(bishop, 0, 6, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 6, 0, ActionType.Move),
+                new Action(friendlyPawn2, 1, 2, ActionType.Move),
+                new Action(friendlyPawn1, 0, 1, ActionType.PawnPromote)
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should not be able to move past a friendly piece (north east)
+        /// </summary>
+        [Fact]
+        public void BishopObstructionNorthEast()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var friendlyPawn1 = new Pawn(TeamColour.White, 1, 5);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(1, 5, friendlyPawn1);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 0, 0, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Move),
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 6, 6, ActionType.Move),
+                new Action(bishop, 7, 7, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 6, 0, ActionType.Move),
+                new Action(friendlyPawn1, 0, 5, ActionType.PawnPromote),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should not be able to move past a friendly piece (south west)
+        /// </summary>
+        [Fact]
+        public void BishopObstructionSouthWest()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var friendlyPawn1 = new Pawn(TeamColour.White, 5, 1);
+            var friendlyPawn2 = new Pawn(TeamColour.White, 4, 2);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(5, 1, friendlyPawn1);
+            gameboard.SetTestBoard(4, 2, friendlyPawn2);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Move),
+                new Action(bishop, 0, 0, ActionType.Move),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 6, 6, ActionType.Move),
+                new Action(bishop, 7, 7, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 1, 5, ActionType.Move),
+                new Action(bishop, 0, 6, ActionType.Move),
+                new Action(friendlyPawn2, 3, 2, ActionType.Move),
+                new Action(friendlyPawn1, 4, 1, ActionType.Move),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should not be able to move past a friendly piece (south east)
+        /// </summary>
+        [Fact]
+        public void BishopObstructionSouthEast()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var friendlyPawn1 = new Pawn(TeamColour.White, 6, 6);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(6, 6, friendlyPawn1);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Move),
+                new Action(bishop, 0, 0, ActionType.Move),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 1, 5, ActionType.Move),
+                new Action(bishop, 0, 6, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 6, 0, ActionType.Move),
+                new Action(friendlyPawn1, 5, 6, ActionType.Move),
+                new Action(friendlyPawn1, 4, 6, ActionType.PawnDoubleMove),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should be able to capture an enemy piece and not move past it (north west)
+        /// </summary>
+        [Fact]
+        public void BishopCaptureNorthWest()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var enemyPawn = new Pawn(TeamColour.Black, 1, 1);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(1, 1, enemyPawn);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Capture),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 6, 6, ActionType.Move),
+                new Action(bishop, 7, 7, ActionType.Move),
+                new Action(bishop, 6, 0, ActionType.Move),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 1, 5, ActionType.Move),
+                new Action(bishop, 0, 6, ActionType.Move),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should be able to capture an enemy piece and not move past it (north east)
+        /// </summary>
+        [Fact]
+        public void BishopCaptureNorthEast()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var enemyKnight = new Knight(TeamColour.Black, 2, 4);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(2, 4, enemyKnight);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Move),
+                new Action(bishop, 0, 0, ActionType.Move),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 6, 6, ActionType.Move),
+                new Action(bishop, 7, 7, ActionType.Move),
+                new Action(bishop, 6, 0, ActionType.Move),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Capture),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should be able to capture an enemy piece and not move past it (south west)
+        /// </summary>
+        [Fact]
+        public void BishopCaptureSouthWest()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var enemyRook = new Rook(TeamColour.Black, 6, 0);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(6, 0, enemyRook);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Move),
+                new Action(bishop, 0, 0, ActionType.Move),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Move),
+                new Action(bishop, 6, 6, ActionType.Move),
+                new Action(bishop, 7, 7, ActionType.Move),
+                new Action(bishop, 6, 0, ActionType.Capture),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 1, 5, ActionType.Move),
+                new Action(bishop, 0, 6, ActionType.Move),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+
+        /// <summary>
+        /// A bishop should be able to capture an enemy piece and not move past it (south east)
+        /// </summary>
+        [Fact]
+        public void BishopCaptureSouthEast()
+        {
+            var gameboard = new Gameboard();
+            var bishop = new Bishop(TeamColour.White, 3, 3);
+            var enemyBishop = new Bishop(TeamColour.Black, 5, 5);
+
+            gameboard.SetTestBoard(3, 3, bishop);
+            gameboard.SetTestBoard(5, 5, enemyBishop);
+            gameboard.CalculateCurrentTeamActions();
+
+            var expected = new List<Action>
+            {
+                new Action(bishop, 2, 2, ActionType.Move),
+                new Action(bishop, 1, 1, ActionType.Move),
+                new Action(bishop, 0, 0, ActionType.Move),
+                new Action(bishop, 4, 4, ActionType.Move),
+                new Action(bishop, 5, 5, ActionType.Capture),
+                new Action(bishop, 6, 0, ActionType.Move),
+                new Action(bishop, 5, 1, ActionType.Move),
+                new Action(bishop, 4, 2, ActionType.Move),
+                new Action(bishop, 2, 4, ActionType.Move),
+                new Action(bishop, 1, 5, ActionType.Move),
+                new Action(bishop, 0, 6, ActionType.Move),
+            }.OrderBy(a => a.ToString()).ToList();
+
+            Assert.Equal(expected, gameboard.CurrentTeamActions);
+        }
+    }
+}
