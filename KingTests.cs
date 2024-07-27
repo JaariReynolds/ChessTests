a95bb44433afeebd6f0e75d9cd40da0e9fbba3af
Jaari <jaari.rey@gmail.com>
2024-07-27 21:27:36 +1000

    updated King tests after implementing legal King moves

--- KingTests.cs
+++ KingTests.cs
@@ -174,27 +174,26 @@ namespace ChessTests
         public void KingWhiteCaptureNorth()
         {
             var gameboard = new Gameboard();
-            var king = new King(TeamColour.White, 3, 3);
-            var enemyPawn1 = new Pawn(TeamColour.Black, 2, 2);
-            var enemyPawn2 = new Pawn(TeamColour.Black, 2, 3);
-            var enemyPawn3 = new Pawn(TeamColour.Black, 2, 4);
+            var king = new King(TeamColour.White, "d5");
+            var enemyPawn1 = new Pawn(TeamColour.Black, "c6");
+            var enemyPawn2 = new Pawn(TeamColour.Black, "d6");
+            var enemyPawn3 = new Pawn(TeamColour.Black, "e6");
 
-            gameboard.SetTestBoard(3, 3, king);
-            gameboard.SetTestBoard(2, 2, enemyPawn1);
-            gameboard.SetTestBoard(2, 3, enemyPawn2);
-            gameboard.SetTestBoard(2, 4, enemyPawn3);
+            gameboard.Board.SetSquare(king);
+            gameboard.Board.SetSquare(enemyPawn1);
+            gameboard.Board.SetSquare(enemyPawn2);
+            gameboard.Board.SetSquare(enemyPawn3);
+
             var actual = gameboard.CalculateTeamActions(TeamColour.White);
 
             var expected = new List<Action>
             {
-                new Action(king, 3, 2, ActionType.Move),
-                new Action(king, 3, 4, ActionType.Move),
-                new Action(king, 4, 2, ActionType.Move),
-                new Action(king, 4, 3, ActionType.Move),
-                new Action(king, 4, 4, ActionType.Move),
-                new Action(king, 2, 2, ActionType.Capture),
-                new Action(king, 2, 3, ActionType.Capture),
-                new Action(king, 2, 4, ActionType.Capture)
+                new Action(king, "c4", ActionType.Move),
+                new Action(king, "d4", ActionType.Move),
+                new Action(king, "e4", ActionType.Move),
+                new Action(king, "c6", ActionType.Capture),
+                new Action(king, "d6", ActionType.Capture),
+                new Action(king, "e6", ActionType.Capture)
             }.OrderBy(a => a.ToString()).ToList();
 
             Assert.Equal(expected, actual);
@@ -234,27 +233,27 @@ namespace ChessTests
         public void KingWhiteCaptureEast()
         {
             var gameboard = new Gameboard();
-            var king = new King(TeamColour.White, 3, 3);
-            var enemyPawn1 = new Pawn(TeamColour.Black, 2, 4);
-            var enemyPawn2 = new Pawn(TeamColour.Black, 3, 4);
-            var enemyPawn3 = new Pawn(TeamColour.Black, 4, 4);
+            var king = new King(TeamColour.White, "d5");
+            var enemyPawn1 = new Pawn(TeamColour.Black, "e6");
+            var enemyPawn2 = new Pawn(TeamColour.Black, "e5");
+            var enemyPawn3 = new Pawn(TeamColour.Black, "e4");
 
-            gameboard.SetTestBoard(3, 3, king);
-            gameboard.SetTestBoard(2, 4, enemyPawn1);
-            gameboard.SetTestBoard(3, 4, enemyPawn2);
-            gameboard.SetTestBoard(4, 4, enemyPawn3);
+            gameboard.Board.SetSquare(king);
+            gameboard.Board.SetSquare(enemyPawn1);
+            gameboard.Board.SetSquare(enemyPawn2);
+            gameboard.Board.SetSquare(enemyPawn3);
+
             var actual = gameboard.CalculateTeamActions(TeamColour.White);
 
             var expected = new List<Action>
             {
-                new Action(king, 2, 2, ActionType.Move),
-                new Action(king, 2, 3, ActionType.Move),
-                new Action(king, 3, 2, ActionType.Move),
-                new Action(king, 4, 2, ActionType.Move),
-                new Action(king, 4, 3, ActionType.Move),
-                new Action(king, 2, 4, ActionType.Capture),
-                new Action(king, 3, 4, ActionType.Capture),
-                new Action(king, 4, 4, ActionType.Capture),
+                new Action(king, "d6", ActionType.Move),
+                new Action(king, "c6", ActionType.Move),
+                new Action(king, "c5", ActionType.Move),
+                new Action(king, "c4", ActionType.Move),
+                new Action(king, "e4", ActionType.Capture),
+                new Action(king, "e5", ActionType.Capture),
+                new Action(king, "e6", ActionType.Capture),
             }.OrderBy(a => a.ToString()).ToList();
 
             Assert.Equal(expected, actual);
@@ -264,27 +263,27 @@ namespace ChessTests
         public void KingWhiteCaptureWest()
         {
             var gameboard = new Gameboard();
-            var king = new King(TeamColour.White, 3, 3);
-            var enemyPawn1 = new Pawn(TeamColour.Black, 2, 2);
-            var enemyPawn2 = new Pawn(TeamColour.Black, 3, 2);
-            var enemyPawn3 = new Pawn(TeamColour.Black, 4, 2);
+            var king = new King(TeamColour.White, "d5");
+            var enemyPawn1 = new Pawn(TeamColour.Black, "c6");
+            var enemyPawn2 = new Pawn(TeamColour.Black, "c5");
+            var enemyPawn3 = new Pawn(TeamColour.Black, "c4");
 
-            gameboard.SetTestBoard(3, 3, king);
-            gameboard.SetTestBoard(2, 2, enemyPawn1);
-            gameboard.SetTestBoard(3, 2, enemyPawn2);
-            gameboard.SetTestBoard(4, 2, enemyPawn3);
+            gameboard.Board.SetSquare(king);
+            gameboard.Board.SetSquare(enemyPawn1);
+            gameboard.Board.SetSquare(enemyPawn2);
+            gameboard.Board.SetSquare(enemyPawn3);
+
             var actual = gameboard.CalculateTeamActions(TeamColour.White);
 
             var expected = new List<Action>
             {
-                new Action(king, 2, 3, ActionType.Move),
-                new Action(king, 2, 4, ActionType.Move),
-                new Action(king, 3, 4, ActionType.Move),
-                new Action(king, 4, 3, ActionType.Move),
-                new Action(king, 4, 4, ActionType.Move),
-                new Action(king, 2, 2, ActionType.Capture),
-                new Action(king, 3, 2, ActionType.Capture),
-                new Action(king, 4, 2, ActionType.Capture),
+                new Action(king, "c6", ActionType.Capture),
+                new Action(king, "c5", ActionType.Capture),
+                new Action(king, "c4", ActionType.Capture),
+                new Action(king, "e4", ActionType.Move),
+                new Action(king, "e5", ActionType.Move),
+                new Action(king, "e6", ActionType.Move),
+                new Action(king, "d6", ActionType.Move),
             }.OrderBy(a => a.ToString()).ToList();
 
             Assert.Equal(expected, actual);
